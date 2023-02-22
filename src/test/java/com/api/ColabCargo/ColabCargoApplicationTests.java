e93972b363a7100c5537e188045d0ec7d50bebff
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-02-22 15:44:27 -0300

    initial commit

---
+++ src/test/java/com/api/ColabCargo/ColabCargoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ColabCargo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColabCargoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
